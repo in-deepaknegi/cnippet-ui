9c5fddecd2b34d8c9be8b475a052f3fcedaa12bd
in-deepaknegi <negideepak.in@gmail.com>
2024-04-07 13:21:01 +0530

    razorpay payment init

--- mols/models/googleUser.js
+++ mols/models/googleUser.js
@@ -10,6 +10,10 @@ const userSchema = new Schema(
             type: String,
             required: true,
         },
+        pro: {
+            type: Boolean,
+            default: false
+        }
     },
     { timestamps: true }
 )
