d02fd095116c98a9ac8d8ed45fedf317d0df8b31
in-deepaknegi <negideepak.in@gmail.com>
2024-04-13 01:00:52 +0530

    razorpay payment success

--- components/routes/home/Pricing.tsx
+++ components/routes/home/Pricing.tsx
@@ -18,9 +18,10 @@ const Pricing = () => {
     const { pro, loading } = FetchPro(email);
     const [msg, setMsg] = useState('Get Access')
 
+
     const router = useRouter();
 
-    const [amount, setAmount] = useState('1');
+    const [amount, setAmount] = useState('499');
     const [currency, setCurrency] = useState('INR');
     const [load, setLoad] = useState(false);
 
@@ -235,37 +236,69 @@ const Pricing = () => {
                                                             Rupee
                                                         </span>
                                                     </p>
-                                                    <button
-                                                        type="submit"
-                                                        className="mt-10 flex items-center justify-center w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                                    >
-                                                        {load && (
-                                                            <svg
-                                                                className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                fill="none"
-                                                                viewBox="0 0 24 24"
-                                                            >
-                                                                <circle
-                                                                    className="opacity-25"
-                                                                    cx="12"
-                                                                    cy="12"
-                                                                    r="10"
-                                                                    stroke="currentColor"
-                                                                    strokeWidth="4"
-                                                                ></circle>
-                                                                <path
-                                                                    className="opacity-75"
-                                                                    fill="currentColor"
-                                                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                                                ></path>
-                                                            </svg>
-                                                        )}
-                                                        {msg}
-                                                    </button>
+                                                    {email ? (
+                                                        <button
+                                                            type="submit"
+                                                            className="mt-10 flex items-center justify-center w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                                        >
+                                                            {load && (
+                                                                <svg
+                                                                    className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                    fill="none"
+                                                                    viewBox="0 0 24 24"
+                                                                >
+                                                                    <circle
+                                                                        className="opacity-25"
+                                                                        cx="12"
+                                                                        cy="12"
+                                                                        r="10"
+                                                                        stroke="currentColor"
+                                                                        strokeWidth="4"
+                                                                    ></circle>
+                                                                    <path
+                                                                        className="opacity-75"
+                                                                        fill="currentColor"
+                                                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                                                    ></path>
+                                                                </svg>
+                                                            )}
+                                                            {msg}
+                                                        </button>
+                                                    ) : (
+                                                        <a href='/login'
+                                                            className="mt-10 flex items-center justify-center w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                                        >
+                                                            {load && (
+                                                                <svg
+                                                                    className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                    fill="none"
+                                                                    viewBox="0 0 24 24"
+                                                                >
+                                                                    <circle
+                                                                        className="opacity-25"
+                                                                        cx="12"
+                                                                        cy="12"
+                                                                        r="10"
+                                                                        stroke="currentColor"
+                                                                        strokeWidth="4"
+                                                                    ></circle>
+                                                                    <path
+                                                                        className="opacity-75"
+                                                                        fill="currentColor"
+                                                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                                                    ></path>
+                                                                </svg>
+                                                            )}
+                                                            Log In
+                                                        </a>
+                                                    )}
 
                                                     <p className="mt-6 text-xs leading-5 text-slate-800 ">
-                                                        Invoices and receipts available for easy company reimbursement
+                                                        {email ? 
+                                                        'Invoices and receipts available for easy company reimbursement' 
+                                                        : 'In order to complete your order, please log in first.'}
                                                     </p>
                                                 </form>
                                             </div>
